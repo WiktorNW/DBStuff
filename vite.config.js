50aef7c04e990c837d5b7eb0c1d1cb343a65d36f
WiktorNW <wik.sokol@gmail.com>
2026-05-22 20:41:12 +0200

    Eviroment Upgrade

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react, { reactCompilerPreset } from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/DBStuffTestVite/',
+  base: '/DBStuff/',
   plugins: [
     react(),
   ],
